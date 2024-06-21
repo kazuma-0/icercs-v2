635181c40d06a9ab6312a950b2ac4b3dc7518afb
Anuj S <hello@kazuma.in>
2024-06-21 23:43:59 +0530

    update

---
+++ src/app/about-coimbatore/carousel.tsx
@@ -0,0 +1,77 @@
+/* eslint-disable @next/next/no-img-element */
+'use client';
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import { useState } from 'react';
+export default function CarouselComponent() {
+  const [index, setIndex] = useState(1);
+  const handleNext = () => {
+    if (index == 17) {
+      setIndex(1);
+      return;
+    }
+    setIndex(index + 1);
+  };
+  const handleBack = () => {
+    if (index == 1) {
+      setIndex(17);
+      return;
+    }
+    setIndex(index - 1);
+  };
+  return (
+    <div className='select-none h-screen flex items-center justify-center w-full container mx-auto py-10'>
+      <div className=' h-full w-full relative'>
+        <div
+          onClick={handleBack}
+          className='cursor-pointer absolute top-0 left-0 h-full w-14 bg-black/40 hover:bg-black/50 transition-all flex items-center justify-center'
+        >
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='24'
+            height='24'
+            viewBox='0 0 24 24'
+            fill='none'
+            stroke='currentColor'
+            stroke-width='2'
+            stroke-linecap='round'
+            stroke-linejoin='round'
+            className='text-white icon icon-tabler icons-tabler-outline icon-tabler-arrow-left'
+          >
+            <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+            <path d='M5 12l14 0' />
+            <path d='M5 12l6 6' />
+            <path d='M5 12l6 -6' />
+          </svg>
+        </div>
+        <div
+          onClick={handleNext}
+          className='cursor-pointer absolute top-0 right-0 h-full w-14 bg-black/40 hover:bg-black/50 transition-all flex items-center justify-center'
+        >
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='24'
+            height='24'
+            viewBox='0 0 24 24'
+            fill='none'
+            stroke='currentColor'
+            stroke-width='2'
+            stroke-linecap='round'
+            stroke-linejoin='round'
+            className='text-white icon icon-tabler icons-tabler-outline icon-tabler-arrow-right'
+          >
+            <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+            <path d='M5 12l14 0' />
+            <path d='M13 18l6 -6' />
+            <path d='M13 6l6 6' />
+          </svg>
+        </div>
+        <img
+          src={`/images/tourism/${index}.jpg`}
+          className='w-full h-full object-cover'
+          alt=''
+        />
+      </div>
+    </div>
+  );
+}
--- src/app/about-coimbatore/page.tsx
+++ src/app/about-coimbatore/page.tsx
@@ -1,46 +1,52 @@
+import CarouselComponent from './carousel';
+
 /* eslint-disable react/no-unescaped-entities */
 export default function AboutKahe() {
   return (
-    <div>
-      <div className='h-[300px]   bg-[url("https://t4.ftcdn.net/jpg/04/91/39/61/360_F_491396168_Wr8iysSxcFnG9VMIJUYOpiUpyl0d85tr.jpg")] bg-center bg-cover overlay flex items-center justify-center'>
-        <h1 className='relative z-20 text-4xl text-white font-bold'>
-          About Coimbatore
-        </h1>
+    <>
+      <div>
+        <div className='h-[300px]   bg-[url("https://t4.ftcdn.net/jpg/04/91/39/61/360_F_491396168_Wr8iysSxcFnG9VMIJUYOpiUpyl0d85tr.jpg")] bg-center bg-cover overlay flex items-center justify-center'>
+          <h1 className='relative z-20 text-4xl text-white font-bold'>
+            About Coimbatore
+          </h1>
+        </div>
+        <p className='text-justify p-5 text-[17px]'>
+          Coimbatore, often hailed as the "Manchester of South India," stands as
+          a beacon of industrial prowess and commercial vibrancy within the
+          Republic of India. Situated at the foothills of the majestic Nilgiri
+          Hills and nestled along the banks of the serene Noyyal River in the
+          western region of Tamil Nadu, this city boasts not only a strategic
+          geographical location but also a serene ambiance enriched by its
+          natural surroundings. With the Western Ghats embracing it from all
+          sides, Coimbatore enjoys a salubrious climate, characterized by
+          moderate temperatures and occasional cool breezes, making it an
+          inviting destination for residents and visitors alike. Renowned as a
+          significant manufacturing and textile hub, Coimbatore is a cornerstone
+          of India's industrial landscape. Its textile industries, ranging from
+          large-scale factories to small and medium enterprises, dominate the
+          economic landscape, fueled by the surrounding cotton fields that
+          contribute to the city's prosperity. In addition to its industrial
+          prominence, Coimbatore is also emerging as a melting pot of cultures
+          and traditions. Its cosmopolitan essence, brought about by a diverse
+          populace, enriches the social fabric and fosters an atmosphere of
+          inclusivity and growth. Moreover, Coimbatore's strategic location
+          renders it easily accessible from all the southern states of India,
+          positioning it as an ideal destination for trade fairs and business
+          conferences. This accessibility enhances its allure as a commercial
+          hub, drawing entrepreneurs, investors, and traders from far and wide.
+          Beyond its economic significance, Coimbatore boasts a rich tapestry of
+          tourist attractions that captivate visitors from across the state and
+          beyond. From historic landmarks to serene natural landscapes, the city
+          offers a plethora of experiences that cater to diverse interests and
+          preferences. In essence, Coimbatore epitomizes the harmonious
+          coexistence of tradition and modernity, poised on the brink of rapid
+          development and progress. With its unique blend of cultural heritage,
+          industrial dynamism, and natural splendor, it is primed to emerge as
+          one of the fastest-growing cities in India, promising boundless
+          opportunities for prosperity and growth.
+        </p>
       </div>
-      <p className='text-justify p-5 text-[17px]'>
-        Coimbatore, often hailed as the "Manchester of South India," stands as a
-        beacon of industrial prowess and commercial vibrancy within the Republic
-        of India. Situated at the foothills of the majestic Nilgiri Hills and
-        nestled along the banks of the serene Noyyal River in the western region
-        of Tamil Nadu, this city boasts not only a strategic geographical
-        location but also a serene ambiance enriched by its natural
-        surroundings. With the Western Ghats embracing it from all sides,
-        Coimbatore enjoys a salubrious climate, characterized by moderate
-        temperatures and occasional cool breezes, making it an inviting
-        destination for residents and visitors alike. Renowned as a significant
-        manufacturing and textile hub, Coimbatore is a cornerstone of India's
-        industrial landscape. Its textile industries, ranging from large-scale
-        factories to small and medium enterprises, dominate the economic
-        landscape, fueled by the surrounding cotton fields that contribute to
-        the city's prosperity. In addition to its industrial prominence,
-        Coimbatore is also emerging as a melting pot of cultures and traditions.
-        Its cosmopolitan essence, brought about by a diverse populace, enriches
-        the social fabric and fosters an atmosphere of inclusivity and growth.
-        Moreover, Coimbatore's strategic location renders it easily accessible
-        from all the southern states of India, positioning it as an ideal
-        destination for trade fairs and business conferences. This accessibility
-        enhances its allure as a commercial hub, drawing entrepreneurs,
-        investors, and traders from far and wide. Beyond its economic
-        significance, Coimbatore boasts a rich tapestry of tourist attractions
-        that captivate visitors from across the state and beyond. From historic
-        landmarks to serene natural landscapes, the city offers a plethora of
-        experiences that cater to diverse interests and preferences. In essence,
-        Coimbatore epitomizes the harmonious coexistence of tradition and
-        modernity, poised on the brink of rapid development and progress. With
-        its unique blend of cultural heritage, industrial dynamism, and natural
-        splendor, it is primed to emerge as one of the fastest-growing cities in
-        India, promising boundless opportunities for prosperity and growth.
-      </p>
-    </div>
+      <CarouselComponent />
+    </>
   );
 }
