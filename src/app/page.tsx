cf7361137d2336e33472b5f4df0d3ff8d839bb4a
mukilan2815 <mukilan2808@gmail.com>
2025-08-18 13:52:09 +0530

    i

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,37 @@ import styles from "./slider.module.css";
 export default function Home() {
   const speakers = [
     {
+      name: "DR. HUI-WEN YANG",
+      title: "Assistant Professor",
+      university: "Department of Biomedical Sciences and Engineering, Tzu-Chi University, Taiwan",
+      image: "https://bmse.tcu.edu.tw/wp-content/uploads/2025/03/9.png", // replace with actual image path
+    },
+
+  
+    {
+      name: "DR. PHAM THI THU THUY",
+      title: "Dean - Faculty of Information Technology, Nha Trang University, Vietnam",
+      university: "",
+      image: "https://canbo.ntu.edu.vn/Staff/PersonelImage/305", // replace with actual image path
+    },
+    {
+      name: "DR. HASIAH MOHAMED",
+      title: "Senior Lecturer, Faculty of Computer and Mathematical Sciences, UiTM Cawangan Terengganu, Malaysia",
+      university: "",
+      image: "https://terengganu.uitm.edu.my/images/FAKULTI/KPPIM/FOTO/HASIAH_MOHAMED__OMAR_Ts_DR.png", // replace with actual image path
+    },
+    {
+      name: "DR. WEN-REN YANG",
+      title: "Associate Professor Director of Artificial Intelligence Applications Center National Changhua University of Education Changhua, Taiwan",
+      university: "",
+      image: "https://media.licdn.com/dms/image/v2/C5603AQE67ZV5E-Lh6w/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1517446614779?e=1758153600&v=beta&t=aIzh6bMXcH0ZUUMJC75MrF1ooEg_jUSYlMuQqula4Ow", // replace with actual image path
+    },
+    {
       name: "PROF. DR. FADI AL-TURJMAN",
       title: "Dean-AI and Informatics",
       university: "Near East University, Turkey",
       image:
-        "https://scholar.googleusercontent.com/citations?view_op=medium_photo&user=5G0uavwAAAAJ&citpid=2", // replace with actual image path
+        "https://scholar.googleusercontent.com/citations?view_op=medium_photo&user=5G0uavwAAAAJ&citpid=2",
     },
     {
       name: "DR. DESHINTA ARROVA DEWI",
@@ -18,7 +44,7 @@ export default function Home() {
       university:
         "Faculty of Data Sciences and Information Technology, INTI International University, Malaysia",
       image:
-        "https://scholar.googleusercontent.com/citations?view_op=medium_photo&user=16XPxHUAAAAJ&citpid=22", // replace with actual image path
+        "https://scholar.googleusercontent.com/citations?view_op=medium_photo&user=16XPxHUAAAAJ&citpid=22",
     },
   ];
 
